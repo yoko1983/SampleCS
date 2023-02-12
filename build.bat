2e18d0b84147a9cdd21cb28b4c023622b0aaf965
yoko1983 <yoko1983@gmail.com>
2023-02-12 16:19:51 +0900

    追加

---
+++ build.bat
@@ -0,0 +1,2 @@
+REM "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" SampleCS.sln /t:build /p:configuration=release /p:Platform="x64"
+MSBuild.exe SampleCS.sln /t:build /p:configuration=Debug /p:Platform="Any CPU"
