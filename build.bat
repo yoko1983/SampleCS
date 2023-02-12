13ffdc95a0653c0da0b40bd416e7800a080eb064
yoko1983 <yoko1983@gmail.com>
2023-02-12 17:10:54 +0900

    dotnet buildに変更

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 REM "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\MSBuild.exe" SampleCS.sln /t:build /p:configuration=release /p:Platform="x64"
-MSBuild.exe SampleCS.sln /t:build /p:configuration=Debug /p:Platform="Any CPU"
+REM MSBuild.exe SampleCS.sln /t:build /p:configuration=Debug /p:Platform="Any CPU"
+dotnet build
